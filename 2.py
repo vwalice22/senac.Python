6722a62721cd6e06074b9b738dccb34554abe81d
vwalice22 <alice.limas.santos@gmail.com>
2025-07-11 20:41:27 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,6 @@
+#comentário de linha
+print("oi") #irá imprimir oi
+#docString
+"""
+comentário por bloco
+"""
---
+++ 3.py
@@ -0,0 +1,3 @@
+print(10,20,30,40, sep=" ", end="\n")
+print(10 ,20,30,40, sep=" ", end="\n")
+print("senac",90,50,40, sep=" ", end="\n")
